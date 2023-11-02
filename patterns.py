6d97374b398a7256a74f35a7635bd647a6de9b85
chandu-chandrakanth <chandusirusala@gmail.com>
2023-11-02 12:32:12 +0530

    xyenta-assQue1-patterns

---
+++ patterns.py
@@ -0,0 +1,35 @@
+
+"""
+1. Print the following patterns based on user inputs
+a. Height of the Pattern ex: n = 4
+#
+##
+###
+####
+b. Modify the pattern to include alignment as parameter.
+c. Ex Height = 4 and Alignment = Right
+     #
+    ##
+   ###
+  ####
+"""
+
+def print_pattern(height, alignment):
+    if alignment == 'Left':
+        for i in range(1, height + 1):
+            print('#' * i)
+    elif alignment == 'Right':
+        for i in range(1, height + 1):
+            left_spaces = ' ' * (height - i)
+            hashes = '#' * i
+            print(left_spaces + hashes)
+    else:
+        print("Invalid alignment. Please use 'Left' or 'Right'.")
+
+height = int(input("Enter the height of the pattern: "))
+alignment = input("Enter the alignment (Left/Right): ").capitalize()
+
+if alignment in ['Left', 'Right']:
+    print_pattern(height, alignment)
+else:
+    print("Invalid alignment. Please use 'Left' or 'Right'.")
